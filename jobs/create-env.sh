38e8bd6fb3f48896b1da6123b347a0a2248124f7
razvanmatisan <razvan.matisan@student.uva.nl>
2024-06-24 12:43:39 +0200

    Update create-env.sh to install fr_core_news_md

--- jobs/create-env.sh
+++ jobs/create-env.sh
@@ -9,4 +9,4 @@ cd $HOME/ieai-miniproject-context-mixing
 python -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-# python -m spacy download fr_core_news_md
+python -m spacy download fr_core_news_md
